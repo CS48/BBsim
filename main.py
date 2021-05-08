479ccd652341c86e2366c253170ead58457b0409
CS48 <calvinstewart.ms@gmail.com>
2021-05-07 21:10:37 -0500

    Got a game to 21 running where the teams take alternating shots

--- main.py
+++ main.py
@@ -19,6 +19,10 @@ last_name_data = ["Smith", "Johnson", "Williams", "Brown", "Jones", "Miller", "D
                   "Hall", "Young", "Allen", "Sanchez", "Wright", "King", "Scott", "Green", "Baker", "Adams", "Nelson",
                   "Hill", "Ramirez", "Campbell", "Mitchell", "Roberts"]
 
+# for making a coin flip based on probability p. Such as an attack that has a 30% probability to work
+def flip(p):
+    return 'H' if random.random() < p else 'T'
+
 # the player class that we will use to build player objects from excel sheet data.
 # it also contains some stats for tracking over the course of a game.
 class Player:
@@ -209,6 +213,317 @@ def show_teams():
         print("No teams exist currently.\n")
 
 
+def game_to_21(active_away, active_home, away_def_assign, home_def_assign, awaypoints, homepoints, home_possession):
+
+    if awaypoints < 21 and homepoints < 21 or abs(awaypoints - homepoints) < 2:
+        if home_possession:
+            shooter = random.choice(list(active_home.values()))
+            defender = away_def_assign[shooter]
+            shot_result = shoot(shooter, defender)
+
+            if shot_result == 2:
+                homepoints = homepoints + 2
+            elif shot_result == 3:
+                homepoints = homepoints + 3
+            elif shot_result == 0:
+                pass
+            else:
+                print("problem with points")
+
+            print("Score:", awaypoints, homepoints)
+
+            return game_to_21(active_away, active_home, away_def_assign, home_def_assign, awaypoints, homepoints, False)
+
+        else:
+            shooter = random.choice(list(active_away.values()))
+            defender = home_def_assign[shooter]
+            shot_result = shoot(shooter, defender)
+
+            if shot_result == 2:
+                awaypoints = awaypoints + 2
+            elif shot_result == 3:
+                awaypoints = awaypoints + 3
+            elif shot_result == 0:
+                pass
+            else:
+                print("problem with points")
+
+            print("Score:", awaypoints, homepoints)
+
+            return game_to_21(active_away, active_home, away_def_assign, home_def_assign, awaypoints, homepoints, True)
+
+    else:
+        if homepoints > awaypoints:
+            print("\nGame Over! Home Wins \nAway:%d Home:%d \n" % (awaypoints, homepoints))
+            return 1
+        else:
+            print("\nGame Over! Away Wins \nAway:%d Home:%d \n" % (awaypoints, homepoints))
+            return 2
+
+#outcomes of a shot attempt: made shot, missed shot, air ball out of bounds, blocked shot
+def shoot(shooter, defender):
+    shot_score = 0
+    def_score = 0
+
+    # first i would like to determine the type of shot that they would most likely shoot based on rating.
+    # For that, I would need to be able to determine how each of a player's three shooting ratings compare to each
+    # other
+
+    # 1 = close, 2 = mid, 3 = three
+    shot_type = [1, 2, 3]
+
+    # a very basic way of deciding whether a player would take more threes vs mid vs close shots
+    if shooter.three >= 80 and shooter.mid_shot <= 80 and shooter.inside_shot <= 80:
+        shot = random.choices(shot_type, weights=(25, 25, 50), k=1)
+    elif shooter.three <= 80 and shooter.mid_shot >= 80 and shooter.inside_shot <= 80:
+        shot = random.choices(shot_type, weights=(25, 50, 25), k=1)
+    elif shooter.three <= 80 and shooter.mid_shot <= 80 and shooter.inside_shot >= 80:
+        shot = random.choices(shot_type, weights=(50, 25, 25), k=1)
+    else:
+        shot = random.choices(shot_type, weights=(33, 33, 33), k=1)
+
+    # I'm gonna do the same thing that i did with attacking in the vb sim
+    # The shooter gets put into a qualitative category based on their relevant
+    # shooting stat (0 = awful, 1 = bad, 2 = average, 3 = good, 4 = great). The same
+    # is done for the defender. Then we determine the chance of the shooter
+    # making the shot based off of that comparison.
+
+    # determining shooter category
+    # inside shot
+    if shot[0] == 1:
+        # output for console
+        print("%s %s shoots from close range" % (shooter.firstname, shooter.lastname))
+
+        if 0 <= shooter.inside_shot < 20:
+            shot_score = shot_score + 0
+        elif 20 <= shooter.inside_shot < 40:
+            shot_score = shot_score + 1
+        elif 40 <= shooter.inside_shot < 60:
+            shot_score = shot_score + 2
+        elif 60 <= shooter.inside_shot < 80:
+            shot_score = shot_score + 3
+        elif 80 <= shooter.inside_shot <= 100:
+            shot_score = shot_score + 4
+        else:
+            print("shot_score error: stat not between 0 and 100")
+
+        if 0 <= defender.interior_d < 20:
+            def_score = def_score + 0
+        elif 20 <= defender.interior_d < 40:
+            def_score = def_score + 1
+        elif 40 <= defender.interior_d < 60:
+            def_score = def_score + 2
+        elif 60 <= defender.interior_d < 80:
+            def_score = def_score + 3
+        elif 80 <= defender.interior_d <= 100:
+            def_score = def_score + 4
+        else:
+            print("shot_score error: stat not between 0 and 100")
+
+    # mid range shot
+    elif shot[0] == 2:
+        # output for console
+        print("%s %s shoots from mid range" % (shooter.firstname, shooter.lastname))
+
+        if 0 <= shooter.mid_shot < 20:
+            shot_score = shot_score + 0
+        elif 20 <= shooter.mid_shot < 40:
+            shot_score = shot_score + 1
+        elif 40 <= shooter.mid_shot < 60:
+            shot_score = shot_score + 2
+        elif 60 <= shooter.mid_shot < 80:
+            shot_score = shot_score + 3
+        elif 80 <= shooter.mid_shot <= 100:
+            shot_score = shot_score + 4
+        else:
+            print("shot_score error: stat not between 0 and 100")
+
+        if 0 <= ((defender.perimeter_d + defender.interior_d)/2) < 20:
+            def_score = def_score + 0
+        elif 20 <= ((defender.perimeter_d + defender.interior_d)/2) < 40:
+            def_score = def_score + 1
+        elif 40 <= ((defender.perimeter_d + defender.interior_d)/2) < 60:
+            def_score = def_score + 2
+        elif 60 <= ((defender.perimeter_d + defender.interior_d)/2) < 80:
+            def_score = def_score + 3
+        elif 80 <= ((defender.perimeter_d + defender.interior_d)/2) <= 100:
+            def_score = def_score + 4
+        else:
+            print("shot_score error: stat not between 0 and 100")
+    # three
+    elif shot[0] == 3:
+        # output for console
+        print("%s %s shoots from three" % (shooter.firstname, shooter.lastname))
+
+        if 0 <= shooter.three < 20:
+            shot_score = shot_score + 0
+        elif 20 <= shooter.three < 40:
+            shot_score = shot_score + 1
+        elif 40 <= shooter.three < 60:
+            shot_score = shot_score + 2
+        elif 60 <= shooter.three < 80:
+            shot_score = shot_score + 3
+        elif 80 <= shooter.three <= 100:
+            shot_score = shot_score + 4
+        else:
+            print("shot_score error: stat not between 0 and 100")
+
+        if 0 <= defender.perimeter_d < 20:
+            def_score = def_score + 0
+        elif 20 <= defender.perimeter_d < 40:
+            def_score = def_score + 0
+        elif 40 <= defender.perimeter_d < 60:
+            def_score = def_score + 0
+        elif 60 <= defender.perimeter_d < 80:
+            def_score = def_score + 0
+        elif 80 <= defender.perimeter_d <= 100:
+            def_score = def_score + 0
+        else:
+            print("shot_score error: stat not between 0 and 100")
+
+    else:
+        print("Error in deciding type of shot")
+
+    # get the success probability by calling the function and passing in the shot score and def score
+    success_prob = shot_succ_prob(shot_score, def_score)
+
+    # make a coin flip using the success probability to determine if the shot is made.
+    if flip(success_prob) == 'H':
+        # output for console
+        print("It's good!")
+        # the return will be used to determine how many points to give
+        if shot[0] == 1 or shot[0] == 2:
+            return 2
+        elif shot[0] == 3:
+            return 3
+        else:
+            print("error exiting shot flip")
+    else:
+        print("It's a missed shot")
+        return 0
+
+
+# convoluted bullshit. There is 100% a better method than this. Please find it.
+# all it does is determine a success probability for a shot based on the qualitative category of the players' ratings.
+def shot_succ_prob(shot_score, def_score):
+
+    if shot_score == 0 and def_score == 0:
+        success_prob = 0.25
+    elif shot_score == 0 and def_score == 1:
+        success_prob = 0.20
+    elif shot_score == 0 and def_score == 2:
+        success_prob = 0.15
+    elif shot_score == 0 and def_score == 3:
+        success_prob = 0.10
+    elif shot_score == 0 and def_score == 4:
+        success_prob = 0.05
+    elif shot_score == 1 and def_score == 0:
+        success_prob = 0.30
+    elif shot_score == 1 and def_score == 1:
+        success_prob = 0.25
+    elif shot_score == 1 and def_score == 2:
+        success_prob = 0.20
+    elif shot_score == 1 and def_score == 3:
+        success_prob = 0.15
+    elif shot_score == 1 and def_score == 4:
+        success_prob = 0.10
+    elif shot_score == 2 and def_score == 0:
+        success_prob = 0.35
+    elif shot_score == 2 and def_score == 1:
+        success_prob = 0.30
+    elif shot_score == 2 and def_score == 2:
+        success_prob = 0.25
+    elif shot_score == 2 and def_score == 3:
+        success_prob = 0.20
+    elif shot_score == 2 and def_score == 4:
+        success_prob = 0.15
+    elif shot_score == 3 and def_score == 0:
+        success_prob = 0.40
+    elif shot_score == 3 and def_score == 1:
+        success_prob = 0.35
+    elif shot_score == 3 and def_score == 2:
+        success_prob = 0.30
+    elif shot_score == 3 and def_score == 3:
+        success_prob = 0.25
+    elif shot_score == 3 and def_score == 4:
+        success_prob = 0.20
+    elif shot_score == 4 and def_score == 0:
+        success_prob = 0.50
+    elif shot_score == 4 and def_score == 1:
+        success_prob = 0.46
+    elif shot_score == 4 and def_score == 2:
+        success_prob = 0.42
+    elif shot_score == 4 and def_score == 3:
+        success_prob = 0.38
+    elif shot_score == 4 and def_score == 4:
+        success_prob = 0.32
+    else:
+        print("problem with success prob")
+
+    return success_prob
+
+
+# Work in progress. The actually act of simulating a game. Gonna break it down by quarters I guess.
+def play():
+    # make sure there is a teams file before we do anything
+    if path.exists("Teams.xlsx"):
+        pass
+    else:
+        print("There are no teams currently.")
+        return 0
+
+
+    # work in the teams file
+    filename = "Teams.xlsx"
+    workbook = load_workbook(filename=filename)
+    # let the user select the two teams that are gonna play
+    print("Here are the teams that are available:\n")
+    show_teams()
+
+    # select the away team first
+    away_team = input("Who is the away team?: ")
+    # get a valid input
+    while True:
+        if away_team in workbook.sheetnames:
+            away_team = load_team(away_team)
+            break
+        else:
+            print("That team doesn't exist, try again")
+            continue
+    # select home team second
+    home_team = input("Who is the home team?: ")
+    # get a valid input
+    while True:
+        if home_team in workbook.sheetnames:
+            home_team = load_team(home_team)
+            break
+        else:
+            print("That team doesn't exist, try again")
+            continue
+
+    # I'm gonna use a dict to assign players from the team to the 5 active spots on the floor. This will be useful
+    # later if you have teams of more than 5 players
+    active_away = {1: None, 2: None, 3: None, 4: None, 5: None}
+    active_home = {1: None, 2: None, 3: None, 4: None, 5: None}
+
+    # assigning to active dicts
+    for x in range(1, 6):
+        active_away[x] = away_team[x - 1]
+        active_home[x] = home_team[x - 1]
+
+    # So this is just an idea, but i thought it would be cool to be able to assign defenders. So in order to do that,
+    # you need to know what defender to reference whenever a certain player has the ball. I can do this by creating a
+    # dict where the active players on one team are the keys and the active players on the other team are the values.
+    # I'll need one of these for both teams because their assignments may not mirror each other.
+
+    away_def_assign = {active_home[1]: active_away[1], active_home[2]: active_away[2], active_home[3]: active_away[3]
+        , active_home[4]: active_away[4], active_home[5]: active_away[5]}
+    home_def_assign = {active_away[1]: active_home[1], active_away[2]: active_home[2], active_away[3]: active_home[3]
+        , active_away[4]: active_home[4], active_away[5]: active_home[5]}
+
+    game_to_21(active_away, active_home, away_def_assign, home_def_assign, 0, 0, False)
+
+
 def main():
     # This is effectively a console based menu that keeps running until you exit it
     # the menu options are selected by entering a number that corresponds with that option.
@@ -245,7 +560,7 @@ def main():
                 print("\n")
                 continue
         elif selection == "5":
-            print("needs to be implemented\n")
+            play()
             continue
         elif selection == "6":
             s = input("Are you sure (y/n):")
@@ -257,9 +572,9 @@ def main():
             print("Invalid input, try again.\n")
             continue
 
-# Work in progress. The actually act of simulating a game. Gonna break it down by quarters I guess.
-def play_quarter():
-    pass
+
+
+
 
 
 
